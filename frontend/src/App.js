7b7a90be54043d755b7d6b26d6a069369fd99398
alecx <alecx.sitex@gmail.com>
2020-05-31 01:21:15 +0300

    frontend remounting

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.scss';
+
+
+import Router from './app/router/router'
+
+function App() {
+  
+  return (
+    <Router />
+  )
+}
+
+export default App;
